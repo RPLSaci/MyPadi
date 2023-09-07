bdf48caff478001124f7dcd8870a67885c8f1291
MoonLGH <MoonLGH@users.noreply.github.com>
2023-09-07 01:16:54 +0000

    deploy: ec72cc5e897301d02df39009b95b96213ee09b2f

--- berita.d4b83651.js
+++
@@ -1,5 +0,0 @@
-const globalvar = {
-    api: "https://mypadiapi.vercel.app/api/"
-};
-
-//# sourceMappingURL=berita.d4b83651.js.map
---
+++ berita.f55aa6fe.js
@@ -0,0 +1,5 @@
+const globalvar = {
+    api: "https://8080-rplsaci-mypadiapi-ww8bjc8w9vw.ws-us104.gitpod.io/api/"
+};
+
+//# sourceMappingURL=berita.f55aa6fe.js.map
